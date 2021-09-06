12fe44c6416d04f4b61afb3c324172b132f06a3d
sandeepy <imsandeep@outlook.in>
2021-09-06 09:52:13 +0530

    javascript all basic example

---
+++ praticejavascript.js
@@ -0,0 +1,138 @@
+// document.writeln("hello");
+/* document.write() function */
+// var a=;    /*value is undefined right now*/ 
+// document.write("hello javascript");
+// var name = "sandeep";
+// document.write(name )
+
+/* alert function */
+
+// window.alert("hello england");
+// alert("welcome to website ");
+// alert(name);
+// var x = "hii i m here to see you";
+// window.alert("welcome to my website !!")
+// var number = 9022221076;
+// window.alert(x);
+// window.alert("my mobile number is" + number);
+// document.writeln("hello");
+// var a ="hello \"sandeep\"";
+// document.writeln(a);
+/* undefined varibale */
+// var roll;
+// document.write(roll);
+/* A Varibale declared without a value will have the value undefined*/
+// var name;        /* this are undefined variable*/
+// var lastname;   /* this are undefined variable*/
+/* or try this */
+// var name
+// name = "sandeep";
+// document.write(name);
+// alert(name);
+
+
+//1. Strings are written inside double or single quotes.
+//2. Numbers are written without quotes.
+//3. If you put a number in quotes,it will be treated as a text String.
+
+/* --------------------*/
+// write variables in one line
+// var name = "sandeep", lastname = "yadav", age = 26;
+// document.write(name, lastname, age);
+// alert("welcome" + name);
+// OR 
+// var fname ="sandeep",
+// lname = "yadav",
+// city = "mumbai";
+// alert(city);
+// document.write(fname,lname);
+
+// OR
+// Re-Declaring Variables
+// If you re-declare a Javascript variable,it will not lose its value
+
+// eg:-
+// var name ="hello javascript";
+// var name;
+// document.write(name);
+
+// OR
+// Operators in javascript
+// 1.Arithmetic (+,-,*,/,%(modulus gives remainder),++,--)
+// 2.Conditional (Relational) (<(less than),>(greater than),<=,>=,==(equal to),!=(not equal to),===(equal value & same type),!==(not equal value or not same type))
+// 3.Logical
+// 4.Bitwise
+// 5.Assignment
+
+// or 
+// getting input from user
+// var name = prompt("tell me the code");
+// document.write(name);
+// alert(name);
+
+// OR
+// var warning =prompt("Enter the code:");
+// alert("Success! Welcome",+warning);
+
+//OR 
+// var name =prompt("enter your otp code","Mr");
+// alert("Code is Correct",+name);
+// document.write(name);
+
+
+//OR
+
+// if-else statements  example
+// var a = prompt("Enter your mobile number to Login");
+// if ((a == 9022221076)){
+
+
+//     alert("Successfull Login");
+// document.write("This is Sandeep Mobile Number");
+// }
+// else{
+//     alert("Wrong input !Please Enter Again To Login");
+//     document.write("Wrong Value");
+// }
+
+
+//  else -if statements
+
+
+
+
+// OR 
+// FUNCTIONS IN JAVASCRIPT
+// function sby(){
+//     alert("Welcome To Website");
+// }
+
+// document.write("hello sandeep");
+// sby();     /* calling a function*/
+
+//use of <br>
+// document.write("hello india<br>");
+// document.write("hello england");
+
+
+// FUNCTIONS WITH PARAMETER
+
+
+
+//REST PARAMETER
+// document.write("hello");
+// function sby(...args){
+//     document.write(args);
+// }
+// sby(10,20,'tim');
+
+// A variable can be used before it has been declared 
+// see the below example
+// a=10;
+// document.write(a);
+// var a;   
+
+
+// take input from user in array
+
+ 
